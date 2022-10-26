464a6844b2dcd0887d35f45d8d2f67f39ccc0391
Shubham Latake <85818959+Shubham-2899@users.noreply.github.com>
2022-10-26 22:33:08 +0530

    added insta link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,19 +9,21 @@ import { Link } from '@mui/material';
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className='socialMedia'>
-         <InstagramIcon />
+      <div className='socialMedia'>
+        <Link href="https://www.instagram.com/shubham_2899" target="_blank">
+          <InstagramIcon />
+        </Link>
         <Link href="https://github.com/Shubham-2899" target="_blank">
-            <GitHubIcon />
-          </Link>
-          <Link href="https://www.linkedin.com/in/shubhamlatake" target="_blank">
+          <GitHubIcon />
+        </Link>
+        <Link href="https://www.linkedin.com/in/shubhamlatake" target="_blank">
           <LinkedInIcon />
-          </Link>
-          <Link href="mailto:shubhamlatake123@gmail.com" target="_blank">
+        </Link>
+        <Link href="mailto:shubhamlatake123@gmail.com" target="_blank">
           <EmailIcon />
-          </Link>
-        </div>
-        <p> &copy: 2022 shubhamlatake.com</p>
+        </Link>
+      </div>
+      <p> &copy: 2022 shubhamlatake.com</p>
     </div>
   )
 }
